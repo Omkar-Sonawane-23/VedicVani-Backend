bff646285612e55113b92bc26bcc0529cad0c52c
Omkar Sonawane <130058150+Omkar-Sonawane-23@users.noreply.github.com>
2024-05-10 02:41:02 +0000

    Backend Established

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors')
+const app = express();
+const port = 3001;
+
+app.use(express.static('public'));
+
+app.use(cors());
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`);
+});
+
+app.get('/api/chapter/:chapter/shlok/:shlok', (req, res) => {
+    const file = path.join(__dirname, './Data/bhagavad-gita-data/slok', `bhagavadgita_chapter_${req.params.chapter}_slok_${req.params.shlok}.json`);
+    res.status(200).sendFile(file);
+});
+
+app.get('/api/chapter/:chapter', (req, res) => {
+    const file = path.join(__dirname, './Data/bhagavad-gita-data/chapter/', `bhagavadgita_chapter_${req.params.chapter}.json`);
+    res.status(200).sendFile(file);
+});
